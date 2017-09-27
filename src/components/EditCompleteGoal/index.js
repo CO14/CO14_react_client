d1de99d0dfbe8eb6758e708f7be95a984975d77f
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-09-27 00:14:52 -0600

    Edit Incomplete Goal Started

--- src/components/EditCompleteGoal/index.js
+++ src/components/EditCompleteGoal/index.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
 
 class EditCompleteGoal extends Component {
-  componentDidMount() {
-    window.scrollTo(0, 0);
-  }
+  // componentDidMount() {
+  //   window.scrollTo(0, 0);
+  // }
 
   render() {
+    console.log(this.props.peakInfo);
     return (
       <div>
         EDIT COMPLETE GOAL
